f164eebe04df2299be8788abab2b066be709300f
Lostgrammer <carlos_cmvp@hotmail.com>
2023-06-25 13:32:57 -0500

    aniadiendo componente cliente para entidad persona

---
+++ src/app/cliente/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+    id:number | undefined;
+    nombre:string | undefined;
+    apellidoPaterno:string | undefined;
+    apellidoMaterno:string | undefined;
+    identificacion:string | undefined;
+}
