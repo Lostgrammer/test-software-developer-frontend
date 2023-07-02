62307fe4995776aa3784c417026c0d8681ae8bf5
Lostgrammer <carlos_cmvp@hotmail.com>
2023-07-02 07:11:41 -0500

    borrando archivo json de prueba de datos no necesario

--- src/app/cliente/cliente.service.ts
+++ src/app/cliente/cliente.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs'
